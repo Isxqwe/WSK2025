601c4b292bdaaf835da91a75307de8ec72a0f6a4
Gustavo <109548468+GustaGitDev@users.noreply.github.com>
2025-09-19 11:37:14 -0300

    giro 450

---
+++ src/main/java/frc/robot/commands/auto/Giro450.java
@@ -0,0 +1,25 @@
+package frc.robot.commands.auto;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.RobotContainer;
+import frc.robot.commands.driveCommands.RotateToAngleWithPID;
+import frc.robot.subsystems.DriveTrain;
+
+public class Giro450 extends SequentialCommandGroup {
+
+    private static final DriveTrain drive = RobotContainer.driveTrain;
+
+    public Giro450() {
+
+        addCommands(
+
+                // Gira para 90 graus, com erro de 1 grau
+                new RotateToAngleWithPID(179.0, 1).withTimeout(3), new RotateToAngleWithPID(179.0, 1).withTimeout(3),
+                new RotateToAngleWithPID(92.0, 1).withTimeout(3)
+        // Timeout usado caso o comando não termine
+
+        );
+
+    }
+}
